88532c5dad0964a4c2e051f5f94024db30a43401
Phoenix1747 <apertured@public-files.de>
2020-11-13 21:15:31 +0100

    increase distance resolution

--- src/gui/distance.gd
+++ src/gui/distance.gd
@@ -4,4 +4,4 @@ onready var node = get_tree().get_root().get_node("Main")
 
 func _process(delta):
 	var dist = (node.p0 - node.p1).length()
-	text = "Distance = " + str(stepify(dist / core.a0,0.0001)) + " a0"
+	text = "Distance = " + str(stepify(dist / core.a0,0.00001)) + " a0"
--- src/gui/mindistance.gd
+++ src/gui/mindistance.gd
@@ -14,5 +14,5 @@ func _process(delta):
 	var dist = (node.p0 - node.p1).length()
 	
 	if dist < mind or mind == -1:
-		text = "Min distance = " + str(stepify(dist / core.a0,0.0001)) + " a0"
+		text = "Min distance = " + str(stepify(dist / core.a0,0.00001)) + " a0"
 		mind = dist
